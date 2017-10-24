d9fef2671544b3382b0ac77f31aa772e5358e3ca
Andy Engle <aengle@salesforce.com>
2017-10-24 12:57:18 -0400

    Reset App.js.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
     render() {
         return (
             <div className="App">
-                Hello world.
+                Your code goes here.
             </div>
         );
     }
